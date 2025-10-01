ef7a535283c99a9dd5ba888e28bbe6b37377b42d
robert1312-dotcom <189187086+robert1312-dotcom@users.noreply.github.com>
2025-10-01 14:53:43 -0500

    sistema corona

---
+++ coronaCircular.py
@@ -0,0 +1,6 @@
+print("Corona Circular")
+radio_ma = float(input("Ingrese radio mayor: "))
+radio_me = float(input("Ingrese radio menor: "))
+area = 3.14*(radio_ma**2 - radio_me**2)
+print("resultados")
+print("Area: ",area)
